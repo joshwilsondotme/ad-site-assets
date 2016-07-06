7411420808ead24fcbf8e557b41a7006a78d3954
Josh Wilson <joshwilsondesign@gmail.com>
2016-07-06 10:06:39 -0400

    new assets

--- dev/webroot/js/script.js
+++ dev/webroot/js/script.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
-  $('.push-img').hpImage({
-    imgParent: '.hp-wrap'
-  });
+
+  ctaResponsive('.ctas');
+
+  // Smooth Scroll with Offset (needed for inline-header)
+  $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - 100 },
+      900, 'swing');
+  	});
+
+  $('.cycle-slide-active .slide-content').addClass('animated fadeIn');
+
+});
+
+/*-----------------------------------------------------------------------------------*/
+/*  Sticky Header
+/*-----------------------------------------------------------------------------------*/
+
+$(window).scroll(function() {
+  $(this).scrollTop() > 100 ? $(".sticky-nav").addClass("sticky") : $(".sticky-nav").removeClass("sticky");
 });
